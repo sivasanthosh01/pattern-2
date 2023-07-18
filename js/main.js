839c703d628d95a604e4c1bf5599918859694508
santhosh <killersanthosh0032@gmail.com>
2023-07-18 15:46:37 +0530

    pattern-2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,15 @@
-for(let i=1;i<=5;i++)
+		// pattern-2
+var n=prompt("enter the N value");
+for(let i=1;i<=n;i++)
 {
-	for(let j=5;j>i;j--)
+	for(let j=n;j>i;j--)
 	{
 		document.write("&nbsp;&nbsp;");	
 	}
 	for(let k=1;k<=i;k++)
 	{
-		document.write(i);
+		document.write("*");
 	}
 		
 	document.write("<br>");
 }
-
-// for(let i=1;i<=5;i++)
-// {
-	// for(let j=i;j<5;j++)
-	// {
-		// document.write("&nbsp;&nbsp;");	
-	// }
-	// for(let k=1;k<=i;k++)
-	// {
-		// document.write(i);
-	// }
-		
-	// document.write("<br>");
-// }
